d274056e8e4e1ff5d488e84cb7bd02e039ef661b
VladOnishchuk <vladyslav.onishchuk@jetsoftpro.com>
2023-04-28 23:21:27 +0300

    Add documentation for API

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from core.yasg import urlpatterns as doc_urls
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include('rest_framework.urls')),
@@ -11,6 +13,7 @@ urlpatterns = [
     path("auth/", include('djoser.urls.jwt')),
     path("api/", include('apps.restaurant.urls')),
 ]
+urlpatterns += doc_urls
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
